1f1738994778d140ee743f34a5c9e1ef15cda9e8
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-11-26 16:10:19 +0500

    Fix style in test.js and bench.js

--- bench.js
+++ bench.js
@@ -2,15 +2,16 @@ const Benchmark = require('benchmark');
 const moment = require('moment-timezone');
 const cctz = require('./build/Release/cctz.node');
 
-const suite = new Benchmark.Suite;
+const suite = new Benchmark.Suite();
 
 suite
 	.add('moment-timezone', () => {
-		const date = moment.tz("2015-09-22 09:35:12", "America/New_York");
+		const date = moment.tz('2015-09-22 09:35:12', 'America/New_York');
 		return date.add(1, 'h').format('YYYY-MM-DD hh-mm-ss');
 	})
 	.add('cctz', () => {
-		return cctz.Bench();
+		const time = cctz.parse('2015-09-22 09:35:12', 'America/New_York');
+		return time.format('%Y-%M-%D %h-%m-%s');
 	})
 	.on('cycle', event => {
 		console.log(String(event.target));
@@ -19,5 +20,5 @@ suite
 		console.log('Fastest is ' + this.filter('fastest').map('name'));
 	})
 	.run({
-		'async': true
+		async: true
 	});
--- test.js
+++ test.js
@@ -1,35 +1,36 @@
 import test from 'ava';
 import bindings from 'bindings';
+
 const cctz = bindings('cctz.node');
 
 test('has time_zone.h methods', t => {
-	t.ok(cctz.time_zone);
-	t.ok(cctz.utc_time_zone);
-	t.ok(cctz.local_time_zone);
-	t.ok(cctz.convert);
-	t.ok(cctz.format);
-	t.ok(cctz.parse);
+	t.is(typeof cctz.time_zone, 'function');
+	t.is(typeof cctz.utc_time_zone, 'function');
+	t.is(typeof cctz.local_time_zone, 'function');
+	t.is(typeof cctz.convert, 'function');
+	t.is(typeof cctz.format, 'function');
+	t.is(typeof cctz.parse, 'function');
 });
 
 test('has civil_time.h methods', t => {
-	t.ok(cctz.civil_second);
-	t.ok(cctz.civil_minute);
-	t.ok(cctz.civil_hour);
-	t.ok(cctz.civil_day);
-	t.ok(cctz.civil_month);
-	t.ok(cctz.civil_year);
-	t.ok(cctz.get_weekday);
-	t.ok(cctz.next_weekday);
-	t.ok(cctz.prev_weekday);
-	t.ok(cctz.get_yearday);
+	t.is(typeof cctz.civil_second, 'function');
+	t.is(typeof cctz.civil_minute, 'function');
+	t.is(typeof cctz.civil_hour, 'function');
+	t.is(typeof cctz.civil_day, 'function');
+	t.is(typeof cctz.civil_month, 'function');
+	t.is(typeof cctz.civil_year, 'function');
+	t.is(typeof cctz.get_weekday, 'function');
+	t.is(typeof cctz.next_weekday, 'function');
+	t.is(typeof cctz.prev_weekday, 'function');
+	t.is(typeof cctz.get_yearday, 'function');
 });
 
 test('has civil_time.h constants', t => {
-	t.ok(cctz.weekday.monday);
-	t.ok(cctz.weekday.tuesday);
-	t.ok(cctz.weekday.wednesday);
-	t.ok(cctz.weekday.thursday);
-	t.ok(cctz.weekday.friday);
-	t.ok(cctz.weekday.saturday);
-	t.ok(cctz.weekday.sunday);
+	t.is(typeof cctz.weekday.monday, 'number');
+	t.is(typeof cctz.weekday.tuesday, 'number');
+	t.is(typeof cctz.weekday.wednesday, 'number');
+	t.is(typeof cctz.weekday.thursday, 'number');
+	t.is(typeof cctz.weekday.friday, 'number');
+	t.is(typeof cctz.weekday.saturday, 'number');
+	t.is(typeof cctz.weekday.sunday, 'number');
 });
