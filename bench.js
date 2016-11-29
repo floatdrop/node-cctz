45dadd76c22a79a4ee8fb86b65d60eaf44468d07
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-11-29 18:50:16 +0500

    add add-hour bench

--- bench.js
+++ bench.js
@@ -3,17 +3,29 @@ const moment = require('moment-timezone');
 const cctz = require('./');
 
 const suite = new Benchmark.Suite();
+const tz = cctz.load_time_zone('America/New_York');
 
 suite
-	.add('Parse-Format (moment)', () => {
+	.add('Parse-Format    (moment)', () => {
 		const date = moment.tz('2015-09-22 09:35:12', 'America/New_York');
 		return date.format('YYYY-MM-DD hh-mm-ss');
 	})
-	.add('Parse-Format   (cctz)', () => {
-		// Cache dis to get +100000 op/s
-		const tz = cctz.load_time_zone('America/New_York');
+	.add('Parse-Format      (cctz)', () => {
 		const time = cctz.parse('%Y-%m-%d %H:%M:%S', '2015-09-22 09:35:12', tz);
 		return cctz.format('%Y-%m-%d %H:%M:%S', time, tz);
+	})
+	.add('Increment hour  (moment)', () => {
+		const date = moment.tz('2015-09-22 09:35:12', 'America/New_York');
+		for (let i = 0; i < 1000; i++) {
+			date.add(i, 'h');
+		}
+	})
+	.add('Increment hour    (cctz)', () => {
+		const tp = cctz.parse('%Y-%m-%d %H:%M:%S', '2015-09-22 09:35:12', tz);
+		const time = tz.lookup(tp).cs;
+		for (let i = 0; i < 1000; i++) {
+			time.hour += i;
+		}
 	})
 	.on('cycle', event => {
 		console.log(String(event.target));
