26cc3603aaf831ab7ed88d33d695192cbd170f16
Vsevolod Strukchinsky <floatdrop@gmail.com>
2016-11-29 17:28:27 +0500

    :lipstick: bench.js

--- bench.js
+++ bench.js
@@ -5,20 +5,17 @@ const cctz = require('./');
 const suite = new Benchmark.Suite();
 
 suite
-	.add('moment-timezone', () => {
+	.add('Parse-Format (moment)', () => {
 		const date = moment.tz('2015-09-22 09:35:12', 'America/New_York');
 		return date.format('YYYY-MM-DD hh-mm-ss');
 	})
-	.add('cctz', () => {
+	.add('Parse-Format   (cctz)', () => {
 		const tz = cctz.load_time_zone('America/New_York');
 		const time = cctz.parse('%Y-%m-%d %H:%M:%S', '2015-09-22 09:35:12', tz);
 		return cctz.format('%Y-%m-%d %H:%M:%S', time, tz);
 	})
 	.on('cycle', event => {
 		console.log(String(event.target));
-	})
-	.on('complete', function () {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
 	})
 	.run({
 		async: true
